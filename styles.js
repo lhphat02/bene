2a0a2e6bd1c05e9f7517934af2f06e202f903165
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2023-12-13 22:49:45 +0700

    Initial

---
+++ styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    gap: 5,
+    padding: 20,
+    marginTop: 50,
+    backgroundColor: '#fff',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 24,
+    color: '#F5F5F5',
+    marginBottom: 20,
+  },
+});
+
+export default styles;
