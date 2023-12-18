a7e35d6e753b02aa525e7a21a43944b0e103e332
Minhquan257 <minhquan250702@gmail.com>
2023-12-18 10:43:31 +0700

    update noti and property

--- server/model/notifications.mjs
+++ server/model/notifications.mjs
@@ -5,13 +5,29 @@ const notificationSchema = new Mongoose.Schema({
     type: String,
     required: true,
   },
+  sender_id: {
+    type: String,
+    required: true,
+  },
+  receiver_id: {
+    type: String,
+    required: true,
+  },
+  booking_id: {
+    type: String,
+    required: true,
+  },
   message: {
     type: String,
     required: true,
   },
   time_stamp: {
     type: Date,
-    default: Date.now,
+    default: new Date(),
+  },
+  seen: {
+    type: Number,
+    default: 0,
   },
 });
 
