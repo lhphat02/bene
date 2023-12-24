45c8e309837fb16083584563221b472bcdab7323
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2023-12-24 12:53:06 +0700

    update

--- screens/BookingScreen/index.js
+++ screens/BookingScreen/index.js
@@ -1,16 +1,40 @@
-import { useContext } from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { useContext, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import { ThemeContext } from '../../context/ThemeContext';
+import { formatDate, getDayCount } from '../../utils/formatter';
 import getStyles from './styles';
-import Divider from '../../components/Divider';
+
+const dateInput = [
+  {
+    name: 'checkin',
+    placeholder: 'Check In',
+  },
+  {
+    name: 'checkout',
+    placeholder: 'Check Out',
+  },
+];
 
 const BookingScreen = ({ navigation, route }) => {
   const { isDarkMode } = useContext(ThemeContext);
   const { data } = route.params;
   const styles = getStyles(isDarkMode);
+  const [showDatePicker, setShowDatePicker] = useState({
+    checkin: false,
+    checkout: false,
+  });
+  const [date, setDate] = useState({
+    checkin: new Date(),
+    checkout: new Date(),
+  });
+
+  console.log('Checkin date', date.checkin);
+  console.log('Checkout date', date.checkout);
+  console.log('data', data.price_per_night);
 
   return (
     <View style={styles.container}>
@@ -18,28 +42,59 @@ const BookingScreen = ({ navigation, route }) => {
         style={styles.back_button}
         onPress={() => navigation.goBack()}
       >
-        <Ionicons
-          name="arrow-back"
-          size={24}
-          color={isDarkMode ? 'white' : 'black'}
-        />
+        <Ionicons name="arrow-back" size={24} color="white" />
       </TouchableOpacity>
 
       <Text style={styles.header}>BOOKING</Text>
 
-      <ScrollView>
-        <Text style={styles.header}>{data?.property_name || 'House'}</Text>
+      <View style={styles.form}>
+        {dateInput.map((item) => (
+          <View key={item.name} style={styles.date_picker}>
+            <TouchableOpacity
+              style={styles.date_picker_button}
+              onPress={() =>
+                setShowDatePicker({ ...showDatePicker, [item.name]: true })
+              }
+            >
+              <Ionicons name="calendar-sharp" size={24} color="white" />
+              <Text style={styles.button_text}>{item.placeholder}</Text>
+            </TouchableOpacity>
 
-        <Text style={styles.description}>
-          {data?.description || 'Description'}
+            {showDatePicker[item.name] && (
+              <DateTimePicker
+                value={new Date()}
+                mode="date"
+                display="default"
+                onChange={(value) => {
+                  setShowDatePicker({ ...showDatePicker, [item.name]: false });
+                  setDate({
+                    ...date,
+                    [item.name]: value.nativeEvent.timestamp,
+                  });
+                }}
+              />
+            )}
+            {date[item.name] && (
+              <Text style={styles.date_display}>
+                {formatDate(date[item.name])}
+              </Text>
+            )}
+          </View>
+        ))}
+
+        <Text style={styles.text}>
+          Price per night: {data.price_per_night} $
         </Text>
 
-        <Divider dividerText="Detail" color={isDarkMode ? 'white' : 'black'} />
-      </ScrollView>
+        <Text style={styles.text}>
+          Total: {''}
+          {getDayCount(date.checkin, date.checkout) * data.price_per_night} $
+        </Text>
+      </View>
 
-      <TouchableOpacity style={styles.book_button} onPress={() => {}}>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
         <Ionicons name="calendar-sharp" size={24} color="white" />
-        <Text style={styles.book_button_text}>Book</Text>
+        <Text style={styles.button_text}>Book</Text>
       </TouchableOpacity>
     </View>
   );
--- utils/formatter.js
+++ utils/formatter.js
@@ -4,3 +4,20 @@ export const shortener = (str, len) => {
   }
   return str;
 };
+
+export function formatDate(date) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString('en-US', options);
+}
+
+export function getDayCount(checkinDate, checkoutDate) {
+  const oneDay = 24 * 60 * 60 * 1000;
+
+  const checkinTime = new Date(checkinDate).getTime();
+  const checkoutTime = new Date(checkoutDate).getTime();
+
+  const differenceInDays = Math.round(
+    Math.abs((checkoutTime - checkinTime) / oneDay)
+  );
+  return differenceInDays;
+}
